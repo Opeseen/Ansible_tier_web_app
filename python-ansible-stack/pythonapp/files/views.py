66fcdf6d0e63fccea167f13b1beeae45c88c120b
Opeseen <Opeyemi5055@yahoo.com>
2023-06-17 12:26:34 +0100

    Deployed

--- python-ansible-stack/pythonapp/files/views.py
+++ python-ansible-stack/pythonapp/files/views.py
@@ -12,30 +12,7 @@ def home():
 @views.route('/dashboard')
 def dashboard():
     record = Record.query.all()
-    df = pd.read_csv(r"./files/Sales_invoice_list.csv")
-    w = df.rowone
-    x = df.rowtwo
-    y = df.rowthree
-    z = df.rowfour
-    return render_template('dashboard.html', data1=w, data2=x, data3=y, data4=z, zip=zip)
+    return render_template('dashboard.html', data=record)
 
 
-
-
-# MYSQL Connection Section
-
-# @views.route('/')
-# def home():
-#     return render_template('home.html')
-
-# @views.route('/dashboard')
-# def dashboard():
-#     cursor.execute("SELECT po_number,invoice_number,invoice_date,due_date FROM student.record;")
-#     result = cursor.fetchall()
-#     print(result)
-#     # for row in result:
-    #     print(row[1])
-    #     print(row)
-
-    # return render_template('dashboard.html')
 
