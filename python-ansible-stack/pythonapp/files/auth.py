f0c4f95e950c247b504da08998edfe16a152afc9
Opeseen <Opeyemi5055@yahoo.com>
2023-06-23 12:14:33 +0100

    Deployed

--- python-ansible-stack/pythonapp/files/auth.py
+++ python-ansible-stack/pythonapp/files/auth.py
@@ -8,18 +8,18 @@ auth = Blueprint('auth',__name__)
 @auth.route('/create_po',methods=['GET', 'POST'])
 def create_po():
     if request.method == 'POST':
-        PO_NUM = request.form.get('po_num').title().strip()
-        INV_NUM = request.form.get('inv_num').title().strip()
+        PO_NUM = request.form.get('po_num').upper().strip()
+        INV_NUM = request.form.get('inv_num').upper().strip()
         INV_DATE = datetime.strptime(request.form.get('inv_date'),'%Y-%m-%d').date()
         DUE_DATE = datetime.strptime(request.form.get('due_date'),'%Y-%m-%d').date()
         try:
             new_record= Record(po_number=PO_NUM,invoice_number=INV_NUM,invoice_date=INV_DATE,due_date=DUE_DATE)
             db.session.add(new_record)
             db.session.commit()
-            flash('PO Details Successfully Added...',category='success')
+            flash(f'PO {PO_NUM} Successfully Added...',category='success')
             return redirect(url_for('auth.create_po'))
         except:
-            flash('There was an error while adding the PO...',category='error')
+            flash(f'There was an error while adding the PO {PO_NUM}...',category='error')
        
     
     return render_template('create_po.html')
