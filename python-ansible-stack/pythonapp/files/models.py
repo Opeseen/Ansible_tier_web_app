8c0a169aa7b11087d1d0685ce6196bae7522af7e
Opeseen <Opeyemi5055@yahoo.com>
2023-07-01 15:42:09 +0100

    Deployed

--- python-ansible-stack/pythonapp/files/models.py
+++ python-ansible-stack/pythonapp/files/models.py
@@ -3,6 +3,6 @@ from sqlalchemy.sql import func;
 
 class Record(db.Model):
     id = db.Column(db.Integer,primary_key=True)
-    note = db.Column(db.String(20))
+    note = db.Column(db.String(500))
     status = db.Column(db.String(20),default='UNCOMPLETED')
     date = db.Column(db.DateTime(timezone=True),default=func.now())
