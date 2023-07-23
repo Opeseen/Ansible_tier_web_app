1cea69fd933f01719853b6a535bc5f4a9c3aa4d2
Opeseen <Opeyemi5055@yahoo.com>
2023-07-23 22:31:34 +0100

    Deployed

--- docker/db/setup.sql
+++ docker/db/setup.sql
@@ -1,10 +1,10 @@
--- create the user
-CREATE USER 'admin'@'%' IDENTIFIED BY 'admin4045';
+-- -- create the user
+-- CREATE USER admin  IDENTIFIED BY 'admin4045';
 
--- Grant all priviledge for the user
-GRANT ALL ON *.* TO 'admin'@'%';
+-- -- Grant all priviledge for the user
+-- GRANT ALL ON *.* TO 'admin'@'%';
 
--- create the database
-CREATE DATABASE IF NOT EXISTS flaskdb;
+-- -- create the database
+-- CREATE DATABASE IF NOT EXISTS flaskdb;
 
-FLUSH PRIVILEGES;
+-- FLUSH PRIVILEGES;
