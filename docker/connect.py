b4b95ec3df9a48c7f914dd8a39bb97fdba7ab756
Opeseen <Opeyemi5055@yahoo.com>
2023-07-23 20:35:15 +0100

    Deployed

--- docker/connect.py
+++ docker/connect.py
@@ -3,14 +3,14 @@ import mysql.connector
 try:
     mydb = mysql.connector.connect(
         host="vprodb",
-		user="root",
+		user="horpeyemi",
 		password="admin1234"
 	)
     print('DATABASE CONNECTED')
     
     mycursor = mydb.cursor()
     mycursor.execute("SHOW DATABASES")
-    
+
     for x in mycursor:
         print(x)
 except Exception as e:
