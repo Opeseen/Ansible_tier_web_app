0353648905db4524c59273564d336cf58206ad02
Opeseen <Opeyemi5055@yahoo.com>
2023-07-23 19:59:14 +0100

    Deployed

--- docker/connect.py
+++ docker/connect.py
@@ -7,6 +7,12 @@ try:
 		password="admin1234"
 	)
     print('DATABASE CONNECTED')
+    
+    mycursor = mydb.cursor()
+    mycursor.execute("SHOW DATABASES")
+    
+    for x in mycursor:
+        print(x)
 except Exception as e:
     print('THERE WAS AN ERROR')
     print(e)
